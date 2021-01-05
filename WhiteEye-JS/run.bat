15063731853d54f9d6448336fe85fcaa0ebeaab4
realQuartzi <nightmaresdev@gmail.com>
2021-01-05 09:06:13 +0100

    Added WhiteEye Javascript Edition
    Renamed WhiteEye-Bot to WhiteEye-CS
    Added new WhiteEye-JS written in node.js
    Updated .gitignore for node.js

---
+++ WhiteEye-JS/run.bat
@@ -0,0 +1,1 @@
+node ./src/Bot.js
---
+++ WhiteEye-JS/src/Bot.js
@@ -0,0 +1,146 @@
+require('dotenv').config();
+
+const fs = require('fs');
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+const PREFIX = "!";
+
+const dataDir = './data/';
+
+client.on('ready', () => 
+{
+    console.log(`${client.user.username} has succesfully logged in!`);
+
+    CreateDirectory();
+});
+
+client.on('guildMemberAdd', (user) => 
+{
+    if(CheckWhitelist(user.id, user.guild.id) == false)
+    {
+        user.kick("The User was not Whitelisted on this Server!");
+    }
+});
+
+client.on('message', (message) => 
+{
+    //Check if Message Author is a Bot
+    if(message.author.bot) return;
+
+    if(message.content.startsWith(PREFIX))
+    {
+        const [cmd, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
+
+        if(cmd == "wl-add")
+        {
+            AddWhitelist(args[0], message.channel.guild.id, message.channel);
+        }
+        else if(cmd == "wl-remove")
+        {
+            RemoveWhitelist(args[0], message.channel.guild.id, message.channel);
+        }
+
+
+
+    }
+
+    console.log(message.content);
+});
+
+client.login(process.env.WHITEEYE_BOT_TOKEN);
+
+
+function AddWhitelist(userID, guildID, channel)
+{
+    CreateWhitelist(guildID);
+
+    if(fs.existsSync(dataDir + guildID + ".json"))
+    {
+        let rawData = fs.readFileSync(dataDir + guildID + ".json");
+        var data = JSON.parse(rawData);
+
+        if(!data.userIDs.includes(userID))
+        {
+            data.userIDs.push(userID)
+
+            let json = JSON.stringify(data, null, 2);
+
+            fs.writeFileSync(dataDir + guildID + ".json", json);
+
+            channel.send("The UserID " + userID + " has been added into the Whitelist!");
+        }
+        else channel.send("A User by that ID is already Whitelisted!");
+
+    }
+}
+
+function RemoveWhitelist(userID, guildID, channel)
+{
+    CreateWhitelist(guildID);
+
+    if(fs.existsSync(dataDir + guildID + ".json"))
+    {
+        let rawData = fs.readFileSync(dataDir + guildID + ".json");
+        var data = JSON.parse(rawData);
+
+        if(data.userIDs.includes(userID))
+        {
+            const index = data.userIDs.indexOf(userID);
+            if(index > -1) 
+            {
+                data.userIDs.splice(index,1);
+
+                let json = JSON.stringify(data, null, 2);
+
+                fs.writeFileSync(dataDir + guildID + ".json", json);
+
+                channel.send("The UserID " + userID + " has been removed from the Whitelist!");
+            }
+        }
+        else channel.send("The User by that ID is already **not** Whitelisted!");
+    }
+}
+
+function CheckWhitelist(userID, guildID)
+{
+    if(fs.existsSync(dataDir + guildID + ".json"))
+    {
+        let rawData = fs.readFileSync(dataDir + guildID + ".json");
+        var data = JSON.parse(rawData);
+
+        if(data.userIDs.includes(userID)) return true;
+        console.log(userID + " could not be found in the Whitelist!");
+    } else CreateWhitelist(guildID);
+
+    return false;
+}
+
+function CreateDirectory()
+{
+    var dir = './data';
+
+    if(!fs.existsSync(dir))
+    {
+        console.log('Data Directory not found! Creating new Directory!');
+        fs.mkdirSync(dir);
+    }
+}
+
+function CreateWhitelist(guildID)
+{
+    if(!fs.existsSync(dataDir + guildID + ".json"))
+    {
+        console.log('File for ' + guildID + ' was not found! Creating new File!');
+
+        let data = JSON.stringify(whitelistData, null, 2);
+
+        fs.writeFileSync(dataDir + guildID + ".json", data);
+    }
+
+}
+
+const whitelistData = {
+    userIDs: []
+};
